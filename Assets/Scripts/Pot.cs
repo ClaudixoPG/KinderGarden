d4e336c7ce78ba5b906d4194c2b9c960c9d134ae
Prion6 <gsepulveda17@alumnos.utalca.cl>
2020-11-17 20:09:05 -0300

    ItemSLot -> Pot

---
+++ Assets/Scripts/Pot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Pot : MonoBehaviour, IDropHandler
+{
+    Plant plant;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        var evento = eventData.pointerDrag;
+        if (evento == null) return;
+
+        if (plant != null)
+        {
+            //REVISA SI ES DEL TIPO QUE CORRESPONDE
+            //SI ES DEL TIPO QUE CORRESPONDE, SUPLE NECESIDAD
+            //SINO, RETORNA
+            if (plant.inNeed && plant.needed == evento.GetComponent<UIElement>().delivered)
+            {
+                plant.inNeed = false;
+                plant.needed = Element.Nothing;
+            }
+        }
+        else if(evento.GetComponent<UIElement>().delivered == Element.Seed)
+        {
+            PlantController p = FindObjectOfType<PlantController>();
+            plant = Instantiate(p.GetPlantPrefab(), transform).GetComponent<Plant>();
+        }
+        
+        
+    }
+}
