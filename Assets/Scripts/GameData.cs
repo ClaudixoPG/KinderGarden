936b1c7b3f6127ec0a422d6b58882ebecd085b22
Claudio Rubio <43611630+ClaudixoPG@users.noreply.github.com>
2020-11-17 18:17:20 -0300

    SoundManager

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    private static GameData _i;
+
+    public static GameData i
+    {
+        get
+        {
+            if (_i == null) _i = Instantiate(Resources.Load<GameData>("GameData"));
+            return _i;
+        }
+    }
+    
+    public SoundAudioClip[] soundAudioClipArray;
+
+    [System.Serializable]
+    public class SoundAudioClip
+    {
+        public SoundManager.Sound sound;
+        public AudioClip audioClip;
+    }
+
+    private void Awake()
+    {
+        SoundManager.Initialize();
+    }
+
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SoundManager
+{
+    public enum Sound
+    {
+        Good,
+        Bad,
+        Evolve,
+        Click
+    }
+
+    private static Dictionary<Sound, float> soundTimerDictionary;
+
+    public static void Initialize()
+    {
+        soundTimerDictionary = new Dictionary<Sound, float>();
+        soundTimerDictionary[Sound.Click] = 0;
+    }
+
+
+    public static void PlaySound(Sound sound)
+    {
+        if (CanPlaySound(sound))
+        {
+            GameObject soundGameObject = new GameObject("Sound");
+            AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
+            audioSource.PlayOneShot(GetAudioClip(sound));
+        }
+    }
+
+    private static bool CanPlaySound(Sound sound)
+    {
+        switch (sound)
+        {
+            default:
+                return true;
+            case Sound.Click:
+                if (soundTimerDictionary.ContainsKey(sound))
+                {
+                    float lastTimePlayed = soundTimerDictionary[sound];
+                    float playerMoveTimerMax = .05f;
+                    if (lastTimePlayed + playerMoveTimerMax < Time.time)
+                    {
+                        soundTimerDictionary[sound] = Time.time;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else return false;
+        }
+    }
+
+    public static AudioClip GetAudioClip(Sound sound)
+    {
+        foreach (GameData.SoundAudioClip soundAudioClip in GameData.i.soundAudioClipArray)
+        {
+            if(soundAudioClip.sound == sound)
+            {
+                return soundAudioClip.audioClip;
+            }
+        }
+        Debug.LogError("Sound" + sound + "not found");
+        return null;
+    }
+
+}
