b626dec11cad751af7f3b82681932a34ff785e21
Prion6 <gsepulveda17@alumnos.utalca.cl>
2020-11-17 20:10:33 -0300

    Update Plant.cs
    added previous element class methids and attributes, added SpriteRenderer

--- Assets/Scripts/Plant.cs
+++ Assets/Scripts/Plant.cs
@@ -2,12 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.UIElements;
+using UnityEngine.EventSystems;
 
-public class Plant : Element
+public class Plant : MonoBehaviour, IPointerDownHandler
 {
+    public Sprite[] sprites;
+    public SpriteRenderer renderer;
+
+    public Element needed = Element.Water;
+    public bool inNeed = true;
+    public Timer timer;
+
+    public int clicks = 0;
     public PlantState state = PlantState.Germination;
-    public Sprite[] imgs; 
+
     public bool instanceNext = false;
+
+    private void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        renderer.sprite = sprites[0];
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!inNeed)
+        {
+            clicks++;
+        }
+    }
+
 }
 
 public enum PlantState
